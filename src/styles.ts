f1c69aaf9d381694b3e8c64022258f4de89d6ba3
Kaue <130503651+KaueBulhoes@users.noreply.github.com>
2025-02-10 14:12:41 -0300

    feat: footer

--- src/styles.ts
+++ src/styles.ts
@@ -4,7 +4,8 @@ export const cores = {
   cinza: '#333',
   preto: '#111',
   branca: '#EEE',
-  verde: '#10AC84'
+  verde: '#10AC84',
+  cinzaClaro: '#A3A3A3'
 }
 
 export const GlobalCss = createGlobalStyle`
